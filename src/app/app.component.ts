126c722279fddc776239a482045ac3ba0664574b
gomez-ashleyhermione <hermionegomez49@gmail.com>
2026-02-08 15:29:17 +0800

    Add global styles enhancements with SCSS mixins and custom cursor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ScrollAnimationService } from './services/scroll-animation.service';
+import { CursorService } from './services/cursor.service';
 
 @Component({
   selector: 'app-root',
   template: `
     <app-header></app-header>
-    <app-hero></app-hero>
-    <app-about></app-about>
-    <app-skills></app-skills>
-    <app-projects></app-projects>
-    <app-contact></app-contact>
+    <router-outlet></router-outlet>
     <app-footer></app-footer>
   `,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'UI/UX Designer Portfolio';
+
+  constructor(
+    private scrollAnimation: ScrollAnimationService,
+    private cursor: CursorService
+  ) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.scrollAnimation.initScrollAnimations();
+      this.cursor.init();
+    }, 100);
+  }
+
+  ngOnDestroy(): void {
+    this.scrollAnimation.destroy();
+    this.cursor.destroy();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ParallaxDirective } from './directives/parallax.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
     HeroComponent,
     AboutComponent,
     SkillsComponent,
     ProjectsComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    ParallaxDirective
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
